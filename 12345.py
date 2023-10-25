2393f0c6a5d42342e878842f1caa74af8d462093
c6rvette <144215190+c6rvette@users.noreply.github.com>
2023-10-25 13:45:14 +0300

    Add files via upload

---
+++ 12345.py
@@ -0,0 +1,9 @@
+# try:
+#     a = int(input())
+#     if a == 10:
+#         print('lol')
+#     else:
+#         print('kek')
+# except:
+#     print('debil?')
+#n2
---
+++ main.py
@@ -0,0 +1,26 @@
+# (n1)
+def pos_or_neg(x):
+    x = float(x)
+    if x > 0:
+        return 'positive'
+    elif x < 0:
+        return 'negative'
+    else:
+        return 'zero'
+# (n2)
+# def even_or_odd(z):
+#     z = float(z)
+#     if z % 2 == 0:
+#         return 'even'
+#     else:
+#         return 'odd'
+# (n3) idk how to test this task sry^^
+# n = str(input())
+# print('Number:', n, '\nNumber count:', len(n))
+# (n4)
+# def sum_of_massive(a):
+#     a = [1, 2, 3, 4, 5]
+#     if sum(a) == 15:
+#         return 'correct sum'
+#     else:
+#         return 'incorrect sum'
